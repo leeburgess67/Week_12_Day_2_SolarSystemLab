85d9abdd5c67073f25168bdccf74c458d35e8e70
leeburgess67 <leeburgessmail@gmail.com>
2018-08-07 15:40:12 +0100

    final commit, images added

--- src/views/planet_info_view.js
+++ src/views/planet_info_view.js
@@ -13,10 +13,22 @@ PlanetInfoView.prototype.bindEvents = function () {
 
 PlanetInfoView.prototype.render = function (planet) {
   const planetInfo = document.createElement('p');
-  planetInfo.textContent = `The name of the planet is ${planet.name}, the orbit is ${planet.orbit}, the day is ${planet.day}, the surface area is ${planet.surfaceArea}, the volume is ${planet.volume}, the gravity is ${planet.gravity}, there are ${planet.moons} moons.`
+  planetInfo.textContent = `The name of the planet is ${planet.name}, the orbit is ${planet.orbit}, the day is ${planet.day}, the surface area is ${planet.surfaceArea}, the volume is ${planet.volume}, the gravity is ${planet.gravity}, there are ${planet.moons} moons. `
   // ${planet.image}
   this.container.innerHTML = '';
   this.container.appendChild(planetInfo);
 };
 
+PlanetInfoView.prototype.render = function (planet) {
+ const planetInfo = document.createElement('p');
+ const planetImage = document.createElement('img');
+ planetInfo.textContent = `The name of the planet is ${planet.name}, the orbit is ${planet.orbit}, the day is ${planet.day}, the surface area is ${planet.surfaceArea}, the volume is ${planet.volume}, the gravity is ${planet.gravity}, there are ${planet.moons} moons.`
+ planetImage.src = `${planet.image}`;
+
+ this.container.innerHTML = '';
+
+ this.container.appendChild(planetImage);
+ this.container.appendChild(planetInfo);
+
+};
 module.exports = PlanetInfoView;
