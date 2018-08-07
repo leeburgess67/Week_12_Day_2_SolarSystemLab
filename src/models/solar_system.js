4cfbe9fe9dfe97b5da98f32dfb40f8f3aa0cd95a
leeburgess67 <leeburgessmail@gmail.com>
2018-08-07 13:26:55 +0100

    solar sytem.js sset up

--- src/models/solar_system.js
+++ src/models/solar_system.js
@@ -1,5 +1,20 @@
+const planets = require('../data/planets.js')
+const PubSub = require('../helpers/pub_sub.js')
+
 const SolarSystem = function(planets) {
   this.planets = planets;
 };
 
+SolarSystem.prototype.bindEvents = function () {
+  PubSub.publish('SolarSystem:all-planets-array', this.planets);
+  PubSub.subscribe('SelectView:selected-planet-index', (evt) => {
+    const selectedPlanetIndex = evt.detail;
+    this.publishPlanetDetail(selectedPlanetIndex);
+  });
+};
+
+  SolarSystem.prototype.publishPlanetDetail = function (selectedPlanetIndex) {
+    const selectedPlanet = this.planets[selectedPlanetIndex];
+    PubSub.publish('SolarSystem:selected-planet-object', selectedPlanet);
+  };
 module.exports = SolarSystem;
